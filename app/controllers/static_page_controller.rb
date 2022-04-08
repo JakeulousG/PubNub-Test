1ded3b6edfa2c4bcb8013b51096bfa90a97d78d3
JakeGiangan <jake@kodakollectiv.com>
2022-04-08 17:37:24 +0800

    added support staf  dashboard

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -5,6 +5,8 @@ class StaticPageController < ApplicationController
     elsif user_signed_in? && current_user.account_types == 'admin'
       @unassigned_chats = Chat.where(staff_id: nil)
       @assigned_chats = Chat.where.not(staff_id: nil)
+    elsif user_signed_in? && current_user.account_types == 'support'
+      @chats = Chat.where(staff_id: current_user.id)
     end 
   end
 end
