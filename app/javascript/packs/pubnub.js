ca685826e10e4f97ef0d4c6fb2471ea1448e09d3
JakeGiangan <jake@kodakollectiv.com>
2022-04-11 15:19:20 +0800

    added users list and chatting feature on chat show page

--- app/javascript/packs/pubnub.js
+++ app/javascript/packs/pubnub.js
@@ -1,63 +1,151 @@
-(function() {
-    var channel = $("#chat_title").text()
-    console.log(channel)
-    var staff = $("#chat_staff").text()
-    console.log(staff)
-    var user = $("#chat_user").text()
-    console.log(user)
-    var pubnub = new PubNub({
-        publishKey: 'pub-c-60cd9afe-8af7-459d-9884-50ed1238ea10',
-        subscribeKey: 'sub-c-5d8a9e22-b644-11ec-8c3a-fe86d55faee6',
-        // uuid: "1234567890qwertyuioplkjhgfdsazxcvbnm",
-        secretKey: 'sec-c-NWQxMmM2NzQtNjcwYi00MTQ0LTkwNGItMjg5Nzg0MjllMTY3'
-    });
-    // generate a random username
-    var randomName = function() {
-        var animals = ['pigeon', 'seagull', 'bat', 'owl', 'sparrows', 'robin', 'bluebird', 'cardinal', 'hawk', 'fish', 'shrimp', 'frog', 'whale', 'shark', 'eel', 'seal', 'lobster', 'octopus', 'mole', 'shrew', 'rabbit', 'chipmunk', 'armadillo', 'dog', 'cat', 'lynx', 'mouse', 'lion', 'moose', 'horse', 'deer', 'raccoon', 'zebra', 'goat', 'cow', 'pig', 'tiger', 'wolf', 'pony', 'antelope', 'buffalo', 'camel', 'donkey', 'elk', 'fox', 'monkey', 'gazelle', 'impala', 'jaguar', 'leopard', 'lemur', 'yak', 'elephant', 'giraffe', 'hippopotamus', 'rhinoceros', 'grizzlybear']    
-        var colors = ['silver', 'gray', 'black', 'red', 'maroon', 'olive', 'lime', 'green', 'teal', 'blue', 'navy', 'fuchsia', 'purple'];
+var pubnub = null;
+var me = null;
+var Users = null;
 
-        return colors[Math.floor(Math.random() * colors.length)] + '_' + animals[Math.floor(Math.random() * animals.length)];
+///21
+var channel = $("#chat_title").text()
+console.log(channel)
+
+var $online_users = $('#online-users');
+var $input = $('#chat-input');
+var $output = $('#chat-output');
+
+var User_factory = function() {
+  var user_list = {};
+  var self = this;
+
+  self.remove = function(uuid) {
+    delete user_list[uuid];
+  };
+
+  self.get = function(uuid) {
+    if(user_list.hasOwnProperty(uuid)) {
+      return user_list[uuid];
+    } else {
+      console.error('Trying to retrieve user that is not present.');
     }
-    
-    var me = $('#whoami').text();
-    console.log(me)
-    var $input = $('#chat-input');
-    var $output = $('#chat-output');
-    
-    var pubnub = PUBNUB.init({
-        publish_key: 'demo',
-        subscribe_key: 'demo'
-    });
-    
-    var channel = 'memewarz-lobby-demo-2';
-    
-    $('#chat').submit(function() {
-        pubnub.publish({
-        channel: channel,
-        message: {
-            text: $input.val(),
-            username: me
-        }
-        });
-        $input.val('');
-        return false;
-    });
-    
-    
-    pubnub.subscribe({
-        channel: channel,
-        message: function(data) {
-        
-        var $line = $('<li class="list-group-item"><strong>' + data.username + ':</strong> </span>');
-        var $message = $('<span class="text" />').text(data.text).html();
-        
-        if(data.username == me) {
-            $line.addClass('me');  
-        }
+  };
 
-        $line.append($message);
-        $output.append($line);
-        $output.scrollTop($output[0].scrollHeight);
-        }
+  self.set = function(uuid, data) {
+    if(!user_list.hasOwnProperty(uuid)) {
+      user_list[uuid] = new User(uuid, data);
+    }
+    return user_list[uuid];
+  };
+  
+  self.all = function() {
+    return user_list;
+  }
+
+};
+  
+var User = function(uuid) {
+  var self = this;
+  self.uuid = uuid;
+
+  var $tpl = $('\
+    <li id="' + self.uuid + '" class="list-group-item"> \
+    ' + self.uuid + ' \
+    </li>');
+
+  self.chat = function(text, $target) {
+
+    var $line = $('<li class="list-group-item"><strong>' + self.uuid + ':</strong> </span>');
+    var $message = $('<span class="text" />').text(text).html();
+
+    $line.append($message);
+    $target.append($line);
+    $target.scrollTop($target[0].scrollHeight);
+  };
+
+  self.leave = function() {
+    $tpl.remove();
+  };
+
+  self.init = function() {  
+    $tpl.click(function() {
+      me.private_chat(self);
+      return false;
     });
-})();
+    $('#online-users').append($tpl);
+  };
+  return self;
+};
+
+var Client = function() {
+  
+  var self = new User($("#current_user").text());
+  return self;
+};
+
+var App = function() {
+  
+  Users = new User_factory();
+  me = new Client();
+  console.log(me);
+
+  pubnub = PUBNUB.init({
+    publish_key: 'pub-c-60cd9afe-8af7-459d-9884-50ed1238ea10',
+    subscribe_key: 'sub-c-5d8a9e22-b644-11ec-8c3a-fe86d55faee6',
+    secret_key: 'sec-c-NWQxMmM2NzQtNjcwYi00MTQ0LTkwNGItMjg5Nzg0MjllMTY3',
+    uuid: me.uuid,
+    auth_key: me.uuid,
+    ssl: true
+  });
+  
+  pubnub.grant({
+    channel: channel,
+    auth_key: me.uuid,
+    read: true,
+    write: true,
+    ttl: 0,
+    callback: function() {
+      
+      pubnub.grant({
+        channel: channel + '-pnpres',
+        auth_key: me.uuid,
+        read: true,
+        write: true,
+        ttl: 0,
+        callback: function() {
+          pubnub.subscribe({
+            channel: channel,
+            message: function(data) {
+              if(data.type == 'chat') {    
+                Users.get(data.payload.uuid).chat(data.payload.text, $output);
+              }
+            },
+            presence: function(data) {
+              console.log(data);
+              if(data.action == "join") {
+                Users.set(data.uuid).init();
+              }
+              if(data.action == "leave" || data.action == "timeout") {
+                Users.remove(data.uuid);
+              }
+            }
+          });
+
+          
+          $('#chat').submit(function() {
+            pubnub.publish({
+              channel: channel,
+              message: {
+                type: 'chat',
+                payload: {
+                    text: $input.val(),
+                    uuid: me.uuid
+                }
+              }
+            });
+            $input.val('');
+            return false;
+          });
+          $('#whoami').text(me.uuid);
+        }
+      });
+    }
+  });
+}
+
+App();
